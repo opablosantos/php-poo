25dc0dbcd50c5c94804badead422df1b722ce246
Pablo Santos <opablosantos@gmail.com>
2021-01-04 22:34:26 -0300

    Alterações no index

--- aula05/index.php
+++ aula05/index.php
@@ -9,10 +9,30 @@
 <?php
     require_once 'ContaBanco.php';
 
-    $c1 = new ContaBanco(001, "cc", "Pablo");
-    $c2 = new ContaBanco(002, "cp", "Alana");
-    print_r($c1);
-    print_r($c2);
+    $p1 = new ContaBanco(); //Jubileu
+    $p2 = new ContaBanco(); //Creuza
+    $p1->abrirConta("cc");
+    $p1->setNumConta(1111);
+    $p1->setDono("Jubileu");
+    $p2->abrirConta("cp");
+    $p2->setNumConta(2222);
+    $p2->setDono("Creuza");
+
+    $p1->depositar(300);
+    $p2->depositar(500);
+
+    $p2->sacar(100);
+
+    $p1->pagarMensal();
+    $p2->pagarMensal();
+
+    $p1->sacar(338);
+    $p2->sacar(530);
+    $p1->fecharConta();
+    $p2->fecharConta();
+
+    print_r($p1);
+    print_r($p2);
 ?>
 </pre>
 </body>
