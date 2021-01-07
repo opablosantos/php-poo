d10929f6799efe5392fcc6bf88e1383eb0963c42
Pablo Santos <opablosantos@gmail.com>
2021-01-06 23:36:58 -0300

    Alterações na classe
    Alterei getters e setters para privado. Fiz pequenas alterações em alguns métodos.

--- aula06/ControleRemoto.php
+++ aula06/ControleRemoto.php
@@ -11,68 +11,90 @@ class ControleRemoto implements Controlador {
         $this->tocando = false;
     }
 
-    public function getVolume() {
+    private function getVolume() {
         return $this->volume;
     }
 
-    public function setVolume($volume) {
+    private function setVolume($volume) {
         $this->volume = $volume;
     }
 
-    public function getLigado() {
+    private function getLigado() {
         return $this->ligado;
     }
 
-    public function setLigado($ligado) {
+    private function setLigado($ligado) {
         $this->ligado = $ligado;
     }
 
-    public function getTocando() {
+    private function getTocando() {
         return $this->tocando;
     }
 
-    public function setTocando($tocando) {
+    private function setTocando($tocando) {
         $this->tocando = $tocando;
     }
 
-
     public function ligar() {
-
+        $this->setLigado(true);
     }
 
     public function desligar() {
-
+        $this->setLigado(false);
     }
 
     public function abrirMenu() {
-
+        echo "<p>-------MENU-------</p>";
+        echo "<p>Está ligado?: ". ($this->getLigado()?"SIM":"NÃO") . "</p>";
+        echo "<p>Está tocando?: ". ($this->getTocando()?"SIM":"NÃO") . "</p>";
+        echo "<p>Volume: ". $this->getVolume(). "</p>";
+        for ($i=0; $i <= $this->getVolume(); $i+=10) {
+            echo "|";
+        }
+        echo "<br/>";
     }
 
     public function fecharMenu() {
-
+        echo "<p>Fechando Menu...</p>";
     }
 
     public function maisVolume() {
-
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume() + 5);
+        } else {
+            echo "<p>ERRO! Não posso aumentar o volume</p>";
+        }
     }
 
     public function menosVolume() {
-
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume() - 5);
+        } else {
+            echo "<p>ERRO! Não posso diminuir o volume</p>";
+        }
     }
 
     public function ligarMudo() {
-
+        if ($this->getLigado() && $this->getVolume() > 0) {
+            $this->setVolume(0);
+        }
     }
 
     public function desligarMudo() {
-
+        if ($this->getLigado() && $this->getVolume() == 0) {
+            $this->setVolume(50);
+        }
     }
 
     public function play() {
-
+        if ($this->getLigado() && !($this->getTocando())) {
+            $this->setTocando(true);
+        }
     }
 
     public function pause() {
-        
+        if ($this->getLigado() && $this->getTocando()) {
+            $this->setTocando(false);
+        }
     }
 }
