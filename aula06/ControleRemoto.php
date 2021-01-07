e945bf2a8192781cd2669e69d4f2c7d9fc1980d8
Pablo Santos <opablosantos@gmail.com>
2021-01-06 21:15:00 -0300

    Adicionei o método construtor à classe

--- aula06/ControleRemoto.php
+++ aula06/ControleRemoto.php
@@ -3,4 +3,12 @@ class ControleRemoto {
     private $volume;
     private $ligado;
     private $tocando;
+
+    public function __construct() {
+        $this->volume = 50;
+        $this->ligado = false;
+        $this->tocando = false;
+    }
+
+
 }
