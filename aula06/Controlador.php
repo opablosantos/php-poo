6e77512253d01c389a08438824be4c7eae3a3b10
Pablo Santos <opablosantos@gmail.com>
2021-01-06 21:07:20 -0300

    Adicionei mais métodos à interface

--- aula06/Controlador.php
+++ aula06/Controlador.php
@@ -4,4 +4,10 @@ interface Controlador {
     public function desligar();
     public function abrirMenu();
     public function fecharMenu();
+    public function maisVolume();
+    public function menosVolume();
+    public function ligarMudo();
+    public function desligarMudo();
+    public function play();
+    public function pause();
 }
