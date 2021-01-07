cef60cea411a351702772f0b6e49bd03f0fad640
Pablo Santos <opablosantos@gmail.com>
2021-01-06 23:35:46 -0300

    Adicionei itens ao index

--- aula06/index.php
+++ aula06/index.php
@@ -5,8 +5,15 @@
     <title></title>
 </head>
 <body>
+<h1>Projeto Controle Remoto</h1>
+<pre>
 <?php
-
+    require_once 'ControleRemoto.php';
+    $c = new ControleRemoto();
+    $c->ligar();
+    $c->maisVolume();
+    $c->abrirMenu();
 ?>
+</pre>
 </body>
 </html>
