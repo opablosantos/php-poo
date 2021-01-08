aa4c34092a323d37509ada815eeaa587e0c5a8c8
Pablo Santos <opablosantos@gmail.com>
2021-01-08 13:39:31 -0300

    Criei a classe Lutador e adicionei os atributos

---
+++ aula07/Lutador.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Lutador
+{
+    //Atributos
+    private $nome;
+    private $nacionalidade;
+    private $idade;
+    private $altura;
+    private $peso;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
+
+    //Métodos
+    
+}
