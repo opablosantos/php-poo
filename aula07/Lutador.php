53d6bdb2ebc7828aace65c8e0e5ddcb6d990d724
Pablo Santos <opablosantos@gmail.com>
2021-01-08 13:49:00 -0300

    Adicionei os métodos internos e o método construtor

--- aula07/Lutador.php
+++ aula07/Lutador.php
@@ -15,5 +15,35 @@ class Lutador
     private $empates;
 
     //Métodos
-    
+    function apresentar() {
+
+    }
+
+    function status() {
+
+    }
+
+    function ganharLuta() {
+
+    }
+
+    function perderLuta() {
+
+    }
+
+    function empatarLuta() {
+
+    }
+
+    //Métodos Especiais
+    public function __construct($no, $na, $id, $al, $pe, $vi, $de, $em) {
+        $this->nome = $no;
+        $this->nacionalidade = $na;
+        $this->idade = $id;
+        $this->altura = $al;
+        $this->peso = $pe;
+        $this->vitorias = $vi;
+        $this->derrotas = $de;
+        $this->empates = $em;
+    }
 }
