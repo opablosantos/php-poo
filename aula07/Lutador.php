629536f387e40d4baef762517b7cf7f746845b7a
Pablo Santos <opablosantos@gmail.com>
2021-01-08 13:53:16 -0300

    Adicionei os getters e setters

--- aula07/Lutador.php
+++ aula07/Lutador.php
@@ -46,4 +46,79 @@ class Lutador
         $this->derrotas = $de;
         $this->empates = $em;
     }
+
+    //Getters e setters
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getNacionalidade() {
+        return $this->nacionalidade;
+    }
+
+    public function setNacionalidade($nacionalidade) {
+        $this->nacionalidade = $nacionalidade;
+    }
+
+    public function getIdade() {
+        return $this->idade;
+    }
+
+    public function setIdade($idade) {
+        $this->idade = $idade;
+    }
+
+    public function getAltura() {
+        return $this->altura;
+    }
+
+    public function setAltura($altura) {
+        $this->altura = $altura;
+    }
+
+    public function getPeso() {
+        return $this->peso;
+    }
+
+    public function setPeso($peso) {
+        $this->peso = $peso;
+    }
+
+    public function getCategoria() {
+        return $this->categoria;
+    }
+
+    public function setCategoria($categoria) {
+        $this->categoria = $categoria;
+    }
+
+    public function getVitorias() {
+        return $this->vitorias;
+    }
+
+    public function setVitorias($vitorias) {
+        $this->vitorias = $vitorias;
+    }
+
+    public function getDerrotas() {
+        return $this->derrotas;
+    }
+
+    public function setDerrotas($derrotas) {
+        $this->derrotas = $derrotas;
+    }
+
+    public function getEmpates() {
+        return $this->empates;
+    }
+
+    public function setEmpates($empates) {
+        $this->empates = $empates;
+    }
+
+
 }
