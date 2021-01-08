3f0e546fd0f6cad6d44fe1862eacb0621c6b24d4
Pablo Santos <opablosantos@gmail.com>
2021-01-08 16:24:08 -0300

    Criei o primeiro objeto

--- aula07/index.php
+++ aula07/index.php
@@ -5,8 +5,13 @@
     <title>Ultra Emoji Combat</title>
 </head>
 <body>
+<pre>
 <?php
-echo "<h1>Ultra Emoji Combat</h1>";
+    echo "<h1>Ultra Emoji Combat</h1>";
+    require_once 'Lutador.php';
+    $l = new Lutador("Pretty Boy", "França", 31, 1.75, 90.9, 11, 2, 1);
+    $l->status();
 ?>
+</pre>
 </body>
 </html>
