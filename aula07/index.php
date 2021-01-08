bbda7425abae503ba96c6485049c80cfccb94d28
Pablo Santos <opablosantos@gmail.com>
2021-01-08 16:38:45 -0300

    Criei demais objetos e chamei o método Status de cada um

--- aula07/index.php
+++ aula07/index.php
@@ -9,8 +9,19 @@
 <?php
     echo "<h1>Ultra Emoji Combat</h1>";
     require_once 'Lutador.php';
-    $l = new Lutador("Pretty Boy", "França", 31, 1.75, 90.9, 11, 2, 1);
-    $l->status();
+    $l = array();
+    $l[0] = new Lutador("Pretty Boy", "França", 31, 1.75, 90.9, 11, 2, 1);
+    $l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2, 3);
+    $l[2] = new Lutador("SnapShadow", "EUA", 35, 1.65, 80.9, 12, 2,1);
+    $l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93, 81.6, 13, 0, 2);
+    $l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70, 119.3, 5, 4, 3);
+    $l[5] = new Lutador("Nerdaart", "EUA", 38, 1.81, 105.7, 12, 2, 4);
+    $l[0]->apresentar();
+    $l[1]->status();
+    $l[2]->status();
+    $l[3]->status();
+    $l[4]->status();
+    $l[5]->status();
 ?>
 </pre>
 </body>
