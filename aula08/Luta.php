062443e59e63a14f267d9aae9353b4b8f6de54c1
Pablo Santos <opablosantos@gmail.com>
2021-01-08 20:14:32 -0300

    Adicionei os getters e setters

--- aula08/Luta.php
+++ aula08/Luta.php
@@ -14,6 +14,39 @@ class Luta {
     }
 
     public function lutar() {
-        
+
+    }
+
+    //MÉTODOS ESPECIAIS
+    public function getDesafiado() {
+        return $this->desafiado;
+    }
+
+    public function setDesafiado($desafiado) {
+        $this->desafiado = $desafiado;
+    }
+
+    public function getDesafiante() {
+        return $this->desafiante;
+    }
+
+    public function setDesafiante($desafiante) {
+        $this->desafiante = $desafiante;
+    }
+
+    public function getRounds() {
+        return $this->rounds;
+    }
+
+    public function setRounds($rounds) {
+        $this->rounds = $rounds;
+    }
+
+    public function getAprovada() {
+        return $this->aprovada;
+    }
+
+    public function setAprovada($aprovada) {
+        $this->aprovada = $aprovada;
     }
 }
